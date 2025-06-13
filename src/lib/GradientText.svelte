4dbc6ec98d014f14aae43b5af1fd93ade2042972
DoubleDotStudios <doubledotgames@gmail.com>
2025-06-13 19:34:10 +1200

    added components for gradiented text and titles

---
+++ src/lib/GradientText.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	let { text, style } = $props();
+</script>
+
+<p class="gradient {style}">{text}</p>
---
+++ src/lib/GradientTitle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import GradientText from "./GradientText.svelte";
+
+	let { style, text } = $props();
+</script>
+
+<GradientText {text} style={"font-bold " + style} />
