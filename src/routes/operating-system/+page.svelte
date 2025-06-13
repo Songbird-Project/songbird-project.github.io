ccc020d91e4d04730bf5481ad525870a04ab4c59
DoubleDotStudios <doubledotgames@gmail.com>
2025-06-13 13:32:07 +1200

    began work on os page

--- src/routes/operating-system/+page.svelte
+++ src/routes/operating-system/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
 </script>
 
-<div>hi</div>
+<main class="pt-10">
+	<div class="text-center">
+		<h1 class="text-6xl font-bold pb-3 gradient">SongbirdOS</h1>
+		<span class="italic"
+			>A perfect and elegant Arch-based, rolling release
+			distro.</span
+		>
+	</div>
+</main>
