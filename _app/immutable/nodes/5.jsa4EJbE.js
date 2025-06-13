5982a8286280086e47f2be4d83a154ed4b732629
DoubleDotStudios <DoubleDotStudios@users.noreply.github.com>
2025-06-13 01:32:43 +0000

    deploy: ccc020d91e4d04730bf5481ad525870a04ab4c59

---
+++ _app/immutable/nodes/5.jsa4EJbE.js
@@ -0,0 +1,2 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/69_IOA4Y.js";import{f as e,a as n}from"../chunks/D163Cuo_.js";var r=e(`<main class="pt-10"><div class="text-center"><h1 class="text-6xl font-bold pb-3 gradient">SongbirdOS</h1> <span class="italic">A perfect and elegant Arch-based, rolling release
+			distro.</span></div></main>`);function p(a){var t=r();n(a,t)}export{p as component};
